4dca4025f8f28417d779f0a2570597992ff265e3
Safak TURKELI <sfktrkl@gmail.com>
2021-08-09 23:23:18 +0300

    Navigating around

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from "react-native";
+import { NavigationStackScreenProps } from "react-navigation-stack";
 
 import { GlobalStyles } from '../styles/Global';
 
-export default function Home() {
+// When navigation stack is configured, every screen configured
+// gets a navigation property on the props assigned to it.
+export default function Home({ navigation }: NavigationStackScreenProps) {
+  // When button is pressed pressHandler called, screen will open and
+  // also in the header there will be back button automatically.
+  const pressHandler = () => {
+    // Navigate action will pop back to earlier stack if a component is already mounted.
+    // Otherwise it will push the route to the stack and navigate to it.
+    navigation.navigate('Reviews');
+    // Push action adds a route on top of the stack and navigates forward.
+    // So, it can mount a component multiple times by adding to top of the stack.
+    //navigation.push('Reviews');
+  };
+
   return (
     <View style={GlobalStyles.container}>
       <Text style={GlobalStyles.titleText}>Home screen</Text>
+      <Button title="Go to review with navigate" onPress={pressHandler} />
     </View>
   );
 }
--- screens/Reviews.tsx
+++ screens/Reviews.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { NavigationStackScreenProps } from "react-navigation-stack";
 
 import { GlobalStyles } from '../styles/Global';
 
-export default function Reviews() {
+export default function Reviews({ navigation }: NavigationStackScreenProps) {
+  const pressHandler = () => {
+    // Header provided with the navigator will automatically have a back button
+    // if it is possible to go back from the active screen. Same behaviour
+    // can be executed by calling goBack.
+    navigation.goBack();
+    // Pop action is also can be used to navigate through previous screens in a stacks.
+    // It may take an argument which allows to specify how many screens to pop back.
+    //navigation.pop(2  /* optional */);
+  };
+
   return (
     <View style={GlobalStyles.container}>
       <Text>Reviews screen</Text>
+      <Button title="Go to home with goBack" onPress={pressHandler} />
     </View>
   );
 }
