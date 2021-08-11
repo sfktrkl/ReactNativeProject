0494b0e8aed29744165c066f9f37847169a8a0a4
Safak TURKELI <sfktrkl@gmail.com>
2021-08-12 01:55:14 +0300

    Using Images

--- screens/Reviews.tsx
+++ screens/Reviews.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Image } from 'react-native';
 import { NavigationStackScreenProps } from "react-navigation-stack";
 
-import { GlobalStyles } from '../styles/Global';
+import { GlobalStyles, images } from '../styles/Global';
 import Card from '../shared/Card';
 
 export default function Reviews({ navigation }: NavigationStackScreenProps) {
@@ -23,8 +23,16 @@ export default function Reviews({ navigation }: NavigationStackScreenProps) {
             is passed through an object. */ }
       <Card>
         <Text>{navigation.getParam('title')}</Text>
-        <Text>Rating: {navigation.getParam('rating')}</Text>
         <Text>{navigation.getParam('body')}</Text>
+        <View style={GlobalStyles.rating}>
+          <Text>Rating: </Text>
+          { /* Dynamically output an image according to the rating.
+                Using string concatenation this can not be done since require doesn't
+                get a concatenated string. Hence, define images somewhere else and then
+                use one of them dynamically (in GlobalSytles for example).
+                Get those images (images.ratings) and then select the one to be applied. */ }
+          <Image source={images.ratings[navigation.getParam('rating')]} />
+        </View>
       </Card>
       <Button title="Go to home with goBack" onPress={pressHandler} />
     </View>
--- styles/Global.tsx
+++ styles/Global.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { ImageSourcePropType, StyleSheet } from 'react-native';
 
 // Create some global styles so that they can be used in
 // any component or screen which will be created if needed.
@@ -17,4 +17,28 @@ export const GlobalStyles = StyleSheet.create({
     marginVertical: 8,
     lineHeight: 20,
   },
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
 });
+
+export type Images = {
+  ratings: {
+    [key: string]: ImageSourcePropType
+  }
+}
+
+export const images: Images = {
+  ratings: {
+    '1': require('../assets/rating-1.png'),
+    '2': require('../assets/rating-2.png'),
+    '3': require('../assets/rating-3.png'),
+    '4': require('../assets/rating-4.png'),
+    '5': require('../assets/rating-5.png'),
+  },
+};
